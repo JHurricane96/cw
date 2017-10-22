e8d7e376b4542b7026b0cab5a33bacdd07362da6
JHurricane96 <arun.jhurricane@gmail.com>
2017-10-22 23:46:45 +0530

    First commit

---
+++ map.js
@@ -0,0 +1,244 @@
+var storymap_data = {
+    // "font_css":"stock:opensans-gentiumbook",
+    "calculate_zoom": false,
+    "storymap":{
+        "map_type":"zoomify",
+        "zoomify":{
+            "attribution":"",
+            "height":1692,
+            "path":"myzoom/",
+            "tolerance":0,
+            "width":2244
+        },
+        "slides":[
+            {
+                "date":"",
+                "location":{
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "line":true
+                },
+                "text":{
+                    "headline":"Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+                    "text":"The journey of the Pevensie children to meet Aslan"
+                },
+                "type":"overview"
+            },
+            {
+                "date":"",
+                "location":{ // wardrobe
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":78.5,
+                    "line":false,
+                    "lon":-148,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Lucy enters Narnia",
+                    "text":"In 1940, four siblings - Peter, Susan, Edmund, and Lucy Pevensie - are among many children evacuated from London during World War II to escape the Blitz. They are sent to the countryside to live with professor Digory Kirke. Exploring the professor's house, Lucy finds a wardrobe which doubles as a magic portal to a forest in a land called Narnia."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // lamppost
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":79.5,
+                    "line":false,
+                    "lon":-140,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Tumnus likes tea",
+                    "text":"At a lamppost oddly located in the forest, she meets Tumnus, a faun, who invites her to tea in his home."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // tumnus house
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":81.5,
+                    "line":false,
+                    "lon":-140,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Tumnus spills it all",
+                    "text":"There the faun confesses that he invited her not out of hospitality, but with the intention of betraying her to the White Witch. The witch has ruled Narnia for years, using magic to keep it frozen in a perpetual winter. She has ordered all Narnians to turn in any humans ('Sons of Adam' or 'Daughters of Eve') they come across. But now that he has come to know and like a human, Tumnus repents his original intention and escorts Lucy back to the lamppost."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // wardrobe
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":78.5,
+                    "line":false,
+                    "lon":-148,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Lucy returns",
+                    "text":"Lucy returns through the wardrobe and finds that only a few seconds have passed in normal time during her absence. Her siblings do not believe her story about another world inside the wardrobe, which is now found to have a solid back panel."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // lamppost
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":79.5,
+                    "line":false,
+                    "lon":-140,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Edmund enters",
+                    "text":"During a game of hide-and-seek on some days later, Lucy again passes into Narnia. This time her brother Edmund chances to follow her. He meets Jadis, who calls herself Queen of Narnia. When she learns that he is human and has two sisters and a brother, she places an enchantment on him. She urges him to bring his siblings to her castle, promising in return to make him her heir."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // wardrobe
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":78.5,
+                    "line":false,
+                    "lon":-148,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Does Narnia exist?",
+                    "text":"When Lucy and Edmund return together through the wardrobe, Edmund realizes that the queen he met and the witch Lucy describes are one and the same. He denies to the others that he has been in Narnia at all. Peter and Susan are puzzled by Lucy's insistence, and consult the Professor, who surprises them by taking Lucy's side in the debate of Narnia's existence."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // wardrobe
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":78.5,
+                    "line":false,
+                    "lon":-148,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Everyone enters",
+                    "text":"Soon afterward, all four children enter Narnia together after hiding in the wardrobe to avoid the professor's dour housekeeper, Mrs. Macready. Remembering the winter cold ahead, they take coats from the wardrobe before exploring."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // tumnus house
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":81.5,
+                    "line":false,
+                    "lon":-140,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Oh no Tumnus",
+                    "text":"Lucy guides them to Tumnus's cave, but they find it ransacked, with a notice from Jadis (the White Witch) proclaiming his arrest for harbouring humans."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // beaver den
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":79,
+                    "line":false,
+                    "lon":-119,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Talking beavers",
+                    "text":"A talking beaver intercepts them, proves himself a friend, and hides the children in his den. There, he and Mrs. Beaver tell them of a prophecy that Jadis's power will fail when two Sons of Adam and two Daughters of Eve fill the four thrones at Cair Paravel. Aslan, the great lion and the rightful King, has been absent for many years but is now 'on the move again' in Narnia."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // witch castle
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":83,
+                    "line":false,
+                    "lon":-105,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Edmund betrays",
+                    "text":"Edmund steals away to Jadis's castle, which is filled with statues of Narnian victims she has turned to stone. Jadis is furious when Edmund appears alone and angrier still to learn that Aslan may have returned. She takes him on her sledge to catch the others or to reach Aslan's court before them."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // father christmas
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":74.3,
+                    "line":false,
+                    "lon":-95,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Winter ends",
+                    "text":"Meanwhile, Mr Beaver realises that Edmund has betrayed them, and they set off at once to seek Aslan at the Stone Table. As they travel, the Witch's spell over Narnia begins to break: Father Christmas (who has not been seen in Narnia for a hundred years) arrives with magical presents: a sword for Peter, a horn and a bow with arrows for Susan, a knife and a bottle of healing cordial for Lucy. The snow melts, and winter ends."
+                }
+            },
+            {
+                "date":"",
+                "location":{ // stone table
+                    "icon":"http://maps.gstatic.com/intl/en_us/mapfiles/ms/micons/blue-pushpin.png",
+                    "lat":72,
+                    "line":false,
+                    "lon":-45,
+                    "zoom":0
+                },
+                "media":{
+                    "caption":"",
+                    "credit":"",
+                },
+                "text":{
+                    "headline":"Aslan to the rescue",
+                    "text":"Aslan welcomes the children and the Beavers to his camp near the Stone Table. Upon hearing Edmund's situation, he orders a rescue party of loyal Narnians."
+                }
+            },
+        ]
+    }
+}
